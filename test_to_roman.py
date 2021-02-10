1104adbb8799928c6e13383ef6c641ec783ee645
axel demborg <axel.demborg@volumental.com>
2021-02-10 17:23:22 +0100

    Value error on negative numbers

--- test_to_roman.py
+++ test_to_roman.py
@@ -1,7 +1,13 @@
 import pytest
 
 def to_roman(number: int) -> str:
+    if number < 0:
+        raise ValueError("Negative values not suported")
     return ""
 
 def test_zero():
     assert to_roman(0) == ""
+
+def test_negative():
+    with pytest.raises(ValueError):
+        to_roman(-42)

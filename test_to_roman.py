1a5ec6803a7f84ec76968772b07caec55fedd4cb
axel demborg <axel.demborg@volumental.com>
2021-02-10 17:27:07 +0100

    Single I:s

--- test_to_roman.py
+++ test_to_roman.py
@@ -3,7 +3,7 @@ import pytest
 def to_roman(number: int) -> str:
     if number < 0:
         raise ValueError("Negative values not suported")
-    return ""
+    return "I" * number
 
 def test_zero():
     assert to_roman(0) == ""
@@ -11,3 +11,6 @@ def test_zero():
 def test_negative():
     with pytest.raises(ValueError):
         to_roman(-42)
+
+def test_one():
+    assert to_roman(1) == "I"

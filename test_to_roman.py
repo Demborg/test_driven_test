ed2cbe2e8acc93deffd9744f6e5f41b8ed7aebca
Axel Demborg <axel.demborg@volumental.com>
2021-02-10 22:07:11 +0100

    Handle all(?) numbers

--- test_to_roman.py
+++ test_to_roman.py
@@ -3,13 +3,19 @@ import pytest
 def to_roman(number: int) -> str:
     if number < 0:
         raise ValueError("Negative values not suported")
-    values = [5, 1]
-    names = ["V", "I"]
+    values = [1000, 500, 100, 50, 10, 5, 1]
+    names = ["M", "D", "C", "L", "X", "V", "I"]
     out = ""
-    for i, _ in enumerate(values[:-1]):
+    for i in range(len(values) - 1):
         while number >= values[i]:
             out += names[i]
             number -= values[i]
+        for j in range(i + 1, len(values)):
+            diff = values[i] - values[j]
+            if number >= diff and j % 2 == 0:
+                out += names[j] + names[i]
+                number -= diff
+                break
 
     return out + names[-1] * number
 
@@ -28,3 +34,9 @@ def test_five():
 
 def test_six():
     assert to_roman(6) == "VI"
+
+def test_nine():
+    assert to_roman(9) == "IX"
+
+def test_large():
+    assert to_roman(1918) == "MCMXVIII"

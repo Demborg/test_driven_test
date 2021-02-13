c5f52efb1f975af66c170e266e9f160f4cc60d10
Axel Demborg <axel.demborg@volumental.com>
2021-02-13 13:28:32 +0100

    Test for push and dummy solution

--- test_the_revenge.py
+++ test_the_revenge.py
@@ -1,10 +1,23 @@
+from typing import Any
+
 import pytest
 
 class Queue():
-    @staticmethod
-    def is_empty():
-        return True
+    empty = True
+
+    def is_empty(self) -> bool:
+        return self.empty
+
+    def push(self, item: Any) -> None:
+        self.empty = False
 
 def test_is_empty():
     queue = Queue()
     assert queue.is_empty()
+
+def test_push():
+    queue = Queue()
+    assert queue.is_empty()
+    
+    queue.push(0)
+    assert not queue.is_empty()
